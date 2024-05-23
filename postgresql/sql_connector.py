b5162ec376749eee3050db43d4b8bcca1516fbc0
birukzlab <birukalemu63@gmail.com>
2024-05-23 15:54:09 -0500

    Security

--- postgresql/sql_connector.py
+++ postgresql/sql_connector.py
@@ -1,18 +1,4 @@
-import psycopg2
 
-__cnx = None
-
-def get_sql_connection():
-    global __cnx
-    if __cnx is None or __cnx.closed:
-        __cnx = psycopg2.connect(
-            user='postgres',
-            password='Ethio#2014',
-            host='localhost',
-            port=5432,
-            database='LearnT'
-        )
-    return __cnx
 
 
 
